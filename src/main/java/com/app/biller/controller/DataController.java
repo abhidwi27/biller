bd0a03bf3532818223c633aaef4bc391532dd347
Abhishek Dwivedi <adwived8@in.ibm.com>
2018-04-04 16:11:25 +0530

    Mutiple row update issue fixed

--- src/main/java/com/app/biller/controller/DataController.java
+++ src/main/java/com/app/biller/controller/DataController.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.SystemPropertyUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -127,6 +128,7 @@ public class DataController {
 			return true;
 		} catch(Exception ex)
 		{
+			ex.printStackTrace();
 			return false;
 		}
 		
